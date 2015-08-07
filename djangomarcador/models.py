e3698bcc436df9afeee9083dbbb9b2b935db9fe7
Dan <udevelopd@gmail.com>
2015-08-07 10:15:35 -0500

    Creating Models

--- djangomarcador/models.py
+++ djangomarcador/models.py
@@ -1,3 +1,42 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    """
+
+    """
+    name = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+       varbose_name = 'tag'
+       varbose_name_plural = 'tags'
+       ordering = ['name']
+            
+    def __unicode__(self):
+        return self.name
+
+class BookMark(models.Model):
+    """
+
+    """
+
+    url = models.URLField()
+    title = models.CharField('title', max_length=225)
+    description = models.TextField('description', blank=True)
+    is_public = models.BooleanFiedl('public', default=True)
+    date_created = models.DateTimeField('date_created')
+    date_updated = models.DateTimeField('date_updated')
+    owner = models.ForeignKey(User, vernose_name='owner', 
+        related_name='bookMarks')
+    tags = models.ManyToManyField(Tag, blank=True)
+
+
+    class Meta:
+       varbose_name = 'bookmark'
+       varbose_name_plural = 'bookmarks'
+       ordering = ['-date_created']
+
+    def __unicode__(self):
+        return self.name
+    
