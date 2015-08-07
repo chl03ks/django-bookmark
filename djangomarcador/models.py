0fb033450148aa5aec0cae1ebaa6c8a2b9d0f6a2
Dan <udevelopd@gmail.com>
2015-08-07 10:22:11 -0500

    Adding Methods to models

--- djangomarcador/models.py
+++ djangomarcador/models.py
@@ -1,25 +1,25 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.untils.encoding import python_2_unicode_compatible
+from django.untils.timezone import now
 
-
+@python_2_unicode_compatible
 class Tag(models.Model):
-    """
 
-    """
     name = models.CharField(max_length=50, unique=True)
+
 
     class Meta:
        varbose_name = 'tag'
        varbose_name_plural = 'tags'
        ordering = ['name']
             
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
-class BookMark(models.Model):
-    """
 
-    """
+@python_2_unicode_compatible
+class BookMark(models.Model):
 
     url = models.URLField()
     title = models.CharField('title', max_length=225)
@@ -37,6 +37,13 @@ class BookMark(models.Model):
        varbose_name_plural = 'bookmarks'
        ordering = ['-date_created']
 
-    def __unicode__(self):
-        return self.name
+    def __str__(self):
+        return '%s (%s)' % (self.title, self.url)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.date_created = now()
+        self.date_updated = now()
+        super(BookMark, self).save(*args, **kwargs)
+
     
