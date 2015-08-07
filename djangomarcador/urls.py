ae469143708011b37bc4336d8821fc5e51787ed8
Dan <udevelopd@gmail.com>
2015-08-07 17:18:27 -0500

    urls

--- djangomarcador/urls.py
+++ djangomarcador/urls.py
@@ -2,7 +2,12 @@ from django.conf.urls import url
 
 
 urlpatterns = [
-    url(r'^user/(?P<username>[-\w]+)/$', 'djangomarcador.views.bookmark_user',
-        name='djangomarcador_bookmark_user'),
-    url(r'^$', 'djangomarcador.views.bookmark_list', name='djangomarcador_bookmark_list'),
+    url(r'^user/(?P<username>[-\w]+)/$',
+        'djangomarcador.views.bookmark_user',
+        name='djangomarcador_bookmark_user'
+        ),
+    url(r'^$',
+        'djangomarcador.views.bookmark_list',
+        name='djangomarcador_bookmark_list'
+        ),
 ]
